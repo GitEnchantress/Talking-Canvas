03e6199b3256db21a23fe37863142ef1988a1ae2
GitEnchantress <pragyajyoti117@gmail.com>
2026-03-14 14:09:21 +0530

    Changes Added

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,11 +1,11 @@
-import React, {useEffect,useState} from 'react';
+import React, { useEffect, useState } from 'react';
 import stand from "../images/stand.png";
 import Button from './Home/Button';
 import { client, urlFor } from "../sanity";
 
 function Hero() {
   const [state, setstate] = useState([])
-  const getData = async() => {
+  const getData = async () => {
     const query = `*[_type=="topframe"]{
       _id,
       name,
@@ -14,44 +14,44 @@ function Hero() {
     const posts = await client.fetch(query);
     setstate(posts)
   }
-    useEffect(() => {
-        window.scrollTo(0, 0)
-        getData()
-      }, []);
-    return (
-        <div >
-     <section className="text-gray-600 body-font mt-5 pt-5  home pb-5 " >
-  <div className="container home-container  flex   md:flex-row flex-col items-center">
-    <div className="lg:flex-grow md:w-1/2 lg:pr-24 px-3 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center ">
-      <h1 className=" sm:text-4xl text-3xl mb-4 font-bold text-gray-900 ">Portrait <span className= " text-yellow-500 ">Painting</span> to Tell Your<span className = "text-red-500"> Stories...</span> </h1>
-      <h3 className=" sm:text-1.5xl text-xl mb-3 font-bold text-yellow-500 ">Upload a photo to transform into <span className = "text-red-500">masterpieces</span></h3>
-      <h3 className=" sm:text-2.5xl text-2xl mb-3 font-bold text-black-400 ">JUST IN <span className = "text-red-500"> 15 </span> DAYS !</h3>
-      <h2 className=" sm:text-4xl text-3xl mb-4 font-bold text-gray-900 ">Memories do talk...</h2>
-      <p className="mb-8  sm:text-1xl text-1xl leading-relaxed" >Beyond just taking pictures, let our expert artists turn them into real paintings.
- </p>
-     <div className=' flex   md:flex-row flex-col items-center' >
-     <div class="col-6  btn-container ">
-     <Button/>
-     </div>     
-     </div>
+  useEffect(() => {
+    window.scrollTo(0, 0)
+    getData()
+  }, []);
+  return (
+    <div >
+      <section className="text-gray-600 body-font mt-5 pt-5  home pb-5 " >
+        <div className="container home-container  flex   md:flex-row flex-col items-center">
+          <div className="lg:flex-grow md:w-1/2 lg:pr-24 px-3 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center ">
+            <h1 className=" sm:text-4xl text-3xl mb-4 font-bold text-gray-900 ">Portrait <span className=" text-yellow-500 ">Painting</span> to Tell Your<span className="text-red-500"> Stories...</span> </h1>
+            <h3 className=" sm:text-1.5xl text-xl mb-3 font-bold text-yellow-500 ">Upload a photo to transform into <span className="text-red-500">masterpieces</span></h3>
+            <h3 className=" sm:text-2.5xl text-2xl mb-3 font-bold text-black-400 ">JUST IN <span className="text-red-500"> 15 </span> DAYS !</h3>
+            <h2 className=" sm:text-4xl text-3xl mb-4 font-bold text-gray-900 ">Memories do talk...</h2>
+            <p className="mb-8  sm:text-1xl text-1xl leading-relaxed" >Beyond just taking pictures, let our expert artists turn them into real paintings.
+            </p>
+            <div className=' flex   md:flex-row flex-col items-center' >
+              <div class="col-6  btn-container ">
+                <Button />
+              </div>
+            </div>
+          </div>
+          <div className="lg:max-w-xl book-container lg:w-full md:w-1/2 w-11/12">
+            <div className='row'>
+              {state.map((item) => (
+
+                <div key={item._id} className='col-4 book flex-row'><img src={urlFor(item.image).url()} alt={item.name}></img></div>
+              ))}
+
+            </div>
+            <div className='row'>
+              <div className='col-12'><img src={stand} alt=""></img></div>
+            </div>
+          </div>
+        </div>
+
+      </section>
     </div>
-    <div className="lg:max-w-xl book-container lg:w-full md:w-1/2 w-11/12">
-        <div className='row'>
-        {state.map((item) => (
-          
-            <div key={item._id} className='col-4 book flex-row'><img src={urlFor(item.image).url()} alt={item.name}></img></div>
-        ))}
-            
-        </div>
-        <div className='row'>
-        <div className='col-12'><img src={stand} alt=""></img></div>
-        </div>
-    </div>
-  </div>
- 
-</section>
-        </div>
-    );
+  );
 }
 
 export default Hero;
